45ecb13c58270e567668f4e09ad245f1a25bc58d
imanuelhalim <imanuelhalim1991@gmail.com>
2020-04-04 18:09:38 +1100

    Update ContactUs

--- src/Main/ContactUs.js
+++ src/Main/ContactUs.js
@@ -1,6 +1,46 @@
 import React from "react";
+import Mailto from "reactv16-mailto";
 
 const ContactUs = () => {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [phone, setPhone] = React.useState("");
+  const [message, setMessage] = React.useState("");
+  const [statusSend, setStatusSend] = React.useState(false);
+
+  const handleName = e => {
+    e.preventDefault();
+    setName(e.target.value);
+  };
+
+  const handleEmail = e => {
+    e.preventDefault();
+    setEmail(e.target.value);
+  };
+
+  const handlePhone = e => {
+    e.preventDefault();
+    setPhone(e.target.value);
+  };
+
+  const handleMessage = e => {
+    e.preventDefault();
+    setMessage(e.target.value);
+  };
+
+  const sendEmail = () => {
+    if (name !== "" && email !== "" && phone !== "" && message !== "") {
+      setStatusSend(true);
+    }
+  };
+
+  // const displayMessageSent = () => {
+  //   if (statusSend) {
+
+  //     );
+  //   }
+  // };
+
   return (
     <div>
       <div className="innerhead">
@@ -34,16 +74,7 @@ const ContactUs = () => {
             </div>
           </div>
           <div className="col-md-9 padspce">
-            <form
-              method="post"
-              accept-charset="utf-8"
-              className="contact-form"
-              autocomplete="off"
-              action="/pages/contact_us"
-            >
-              <div style={{ display: "none" }}>
-                <input type="hidden" name="_method" value="POST" />
-              </div>{" "}
+            <form className="contact-form">
               <div className="contactformdiv">
                 <h4>
                   Send us an <span className="txtcolor"> Email</span>
@@ -51,55 +82,56 @@ const ContactUs = () => {
                 <div className="col-md-4">
                   <input
                     type="text"
-                    name="name"
+                    value={name}
                     className="forminputs"
-                    placeholder="Name"
-                    required="required"
+                    placeholder="Enter name"
+                    required
+                    onChange={handleName}
+                  />
+                </div>
+                <div className="col-md-4">
+                  <input
+                    type="email"
+                    value={email}
+                    className="forminputs"
+                    placeholder="Enter email"
+                    required
+                    onChange={handleEmail}
                   />
                 </div>
                 <div className="col-md-4">
                   <input
                     type="text"
-                    name="email"
+                    value={phone}
                     className="forminputs"
-                    placeholder="Email"
-                    required="required"
-                  />{" "}
-                </div>
-                <div className="col-md-4">
-                  <input
-                    type="text"
-                    name="phone"
-                    className="forminputs"
-                    placeholder="Phone"
-                    required="required"
-                  />{" "}
+                    placeholder="Enter phone number"
+                    required
+                    onChange={handlePhone}
+                  />
                 </div>
                 <div className="col-md-12">
                   <div className="input textarea required">
                     <textarea
-                      name="message"
+                      value={message}
                       className="formmsgbox"
                       placeholder="Enter your message"
-                      required="required"
+                      required
                       rows="5"
                       id="message"
-                    ></textarea>
-                  </div>{" "}
+                      onChange={handleMessage}
+                    />
+                  </div>
                 </div>
                 <div className="col-md-12">
-                  <input type="submit" value="SEND" className="sendbg" />
+                  <input
+                    type="submit"
+                    value="SEND"
+                    className="sendbg"
+                    onClick={sendEmail}
+                  />
                 </div>
               </div>
-              <div style={{ display: "none" }}>
-                <input
-                  type="hidden"
-                  name="_Token[fields]"
-                  value="a0eb778a1f4c3eb28ba59bacd229cfba7552b107%3A"
-                />
-                <input type="hidden" name="_Token[unlocked]" value="" />
-              </div>
-            </form>{" "}
+            </form>
           </div>
 
           <div className="col-md-12">
@@ -110,9 +142,9 @@ const ContactUs = () => {
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3146.37977467793!2d145.11590481532255!3d-37.94491757972888!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad66b422c55a2c3%3A0x6bc5a906b6d77401!2sFactory+28%2F266+Osborne+Ave%2C+Clayton+South+VIC+3169%2C+Australia!5e0!3m2!1sen!2sin!4v1549885587604"
                   width="100%"
                   height="230"
-                  frameborder="0"
+                  frameBorder="0"
                   style={{ border: "0" }}
-                  allowfullscreen
+                  allowFullScreen
                 ></iframe>
               </div>
             </div>
