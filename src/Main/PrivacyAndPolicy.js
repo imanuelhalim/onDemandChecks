168378f0122d428c425583fa5e3eb971d9f2d118
imanuelhalim <imanuelhalim1991@gmail.com>
2020-04-25 19:03:42 +1000

    Updated Privacy and Policy

--- src/Main/PrivacyAndPolicy.js
+++ src/Main/PrivacyAndPolicy.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const PrivacyAndPolicy = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div>
       <div className="innerhead">
