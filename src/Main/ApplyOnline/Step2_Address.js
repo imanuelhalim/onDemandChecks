3bdd14adef5ba6eb2850dc783db7697fd9c73d06
imanuelhalim <imanuelhalim1991@gmail.com>
2020-04-25 19:22:15 +1000

    Updated Step2 Address

--- src/Main/ApplyOnline/Step2_Address.js
+++ src/Main/ApplyOnline/Step2_Address.js
@@ -171,27 +171,6 @@ const Step2_Address = (props) => {
               <label>
                 State<span className="required">* </span>
               </label>
-              {/* <div className="input select">
-            <select
-              name="postalstate"
-              className="inpuststyles"
-              id="postalstate"
-            >
-              <option value="">Please Select State</option>
-              <option value="Australian Capital Territory">
-                Australian Capital Territory
-              </option>
-              <option value="New South Wales">New South Wales</option>
-              <option value="Northern Territory">
-                Northern Territory
-              </option>
-              <option value="Queensland">Queensland</option>
-              <option value="South Australia">South Australia</option>
-              <option value="Tasmania">Tasmania</option>
-              <option value="Victoria">Victoria</option>
-              <option value="Western Australia">Western Australia</option>
-            </select>
-          </div>{" "} */}
               <input
                 type="text"
                 name="postalState"
