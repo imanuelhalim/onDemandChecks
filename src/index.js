169a2b113359efbfca1a728fcc6dcdfa3e35236d
imanuelhalim <imanuelhalim1991@gmail.com>
2020-05-13 23:56:42 +1000

    Updated Index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "./App.css";
 import "./style.css";
+import "./authstyle.css";
 import "react-phone-number-input/style.css";
+import Amplify from "aws-amplify";
+import config from "./config.json";
+
+Amplify.configure({
+  Auth: {
+    mandatorysignid: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
